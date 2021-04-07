a0288d56d80c27009bb76ab053a358768e02897d
SerhiiVakoliuk <79380482+s-vakoliuk@users.noreply.github.com>
2021-04-07 22:26:15 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,64 +1,83 @@
 //створити функцію яка приймає масив та виводить його
-function funcArrCars() {
-    let ArrayStrings = ["fiat", "toyota", "peugeot", "citroen", "nissan"];
-    for (let item = 0; item<5; item = item + 1) {
-        console.log(ArrayStrings);
+function funcArrCars(ArrayStrings) {
+    for (let item = 0; item<ArrayStrings.length; item = item + 1) {
+        console.log(ArrayStrings[item]);
     }
-    return(ArrayStrings);
+    return ArrayStrings;
 }
-console.log(funcArrCars());
+funcArrCars(["fiat", "toyota", "peugeot", "citroen", "nissan"]);
 
 // створити функцію яка заповнює масив рандомними числами та виводить його. Для виведення використати попередню функцію.
-function funcArrRandNum(ArrayRandomNumbers = []) {
+function funcArrRandNum(ArrRandomNum) {
+    let ArrayRandomNumbers = [];
     for (let item = 0; item < 5; item = item + 1) {
         ArrayRandomNumbers[item] = Math.floor(Math.random() * 10);
     }
-    return(ArrayRandomNumbers)
+    console.log("Random числа: ",ArrayRandomNumbers);
 }
-console.log(funcArrRandNum());
+funcArrRandNum([]);
 
 // створити функцію яка повертає найменше число з масиву. (Без Math.min!)
-function funcNumMin(ArrNumbers = [10, 20, 30]) {
-    let min = ArrNumbers[0];
+function funcNumMin(ArrNumbers) {
+        let min = ArrNumbers[0];
     for (let item = 0; item < ArrNumbers.length; item = item + 1) {
         if (ArrNumbers[item] < min) {
             min = ArrNumbers[item];
         }
     }
-return(min);
+    console.log("Мінімальне число: ", min);
+    return ArrNumbers;
+
 }
-console.log(funcNumMin());
+funcNumMin([5,9,3,1,7]);
 
 // створити функцію яка повертає найбільше число з масиву (Без Math.max!)
-function funcNumMax (ArrNumbers = [10, 20, 30]) {
+function funcNumMax (ArrNumbers) {
     let max = ArrNumbers[0];
     for (let item = 0; item < ArrNumbers.length; item = item + 1) {
         if (ArrNumbers[item] > max) {
             max = ArrNumbers[item];
         }
     }
-    return(max);
+    console.log("Максимальне число: ", max);
+    return ArrNumbers;
 }
-console.log(funcNumMax());
+funcNumMax([2,5,1,8,9]);
 
 // створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його
-function funcNumSum (ArrNumbers = [10, 20, 30]) {
+function funcNumSum (ArrNumbers) {
     let Sum = 0;
     for (let item = 0; item < ArrNumbers.length; item = item + 1) {
-        Sum= Sum+ArrNumbers[item];
+        Sum = Sum+ArrNumbers[item];
     }
-    return(Sum);
+    console.log("Сума чисел: ", Sum);
+    return ArrNumbers;
 }
-console.log(funcNumSum());
+funcNumSum([2,5,1,8,9]);
 
 //створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-function funcNumAverage (ArrNumbers = [10, 20, 30]) {
+function funcNumAverage (ArrNumbers) {
     let Sum=0;
     let Average = 0;
     for (let item = 0; item < ArrNumbers.length; item = item + 1) {
         Sum= Sum+ArrNumbers[item];
-        Average=(Sum/ArrNumbers.length);
+        Average=Sum/ArrNumbers.length;
     }
-    return(Average);
+    console.log("Середнє значення чисел: ", Average);
+    return ArrNumbers;
 }
- console.log(funcNumAverage());
+funcNumAverage([2,5,1,8,9]);
+
+//Створити функцію яка приймає масив будь яких об'єктів, та повертає масив значень всіх обєктів
+ const arrObjects = [{name: "Vova", age: 17}, {car: null, model: undefined}, {skills: ['html', 'js']}];
+ function getAllKeys(objectArray) {
+     let allKeys = [];
+     for (const object of objectArray) {
+         const keysOfObject = Object.keys(object);
+         for (const key of keysOfObject) {
+             allKeys.push(key);
+         }
+     }
+     return allKeys;
+ }
+ console.log(getAllKeys(arrObjects));
